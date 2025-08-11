59a6b37949ee00ccfdcad826b9048b99475f7d26
Yahia Bajbouj <yahia.bajbouj@gmail.com>
2025-08-11 10:09:15 +0300

    Add relationship to models

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $user = User::find(1);
+    return $user->load(["posts", "posts.comments"]);//->pluck('content');
+    // return view('welcome');
 });
