db9653fe8a89a742e002ac72e5fddf8b4e48a9fa
Yahia Bajbouj <yahia.bajbouj@gmail.com>
2025-08-12 17:28:44 +0300

    with VS load & Eger Loading VS Lazy Loading

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,29 @@
 <?php
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $user = User::find(1);
-    return $user->load(["posts", "posts.comments"]);//->pluck('content');
+    // Load Function:
+    // $posts = Post::all();
+    // $posts->load(["user"]);
+    // foreach ($posts as $post) {
+    //     echo "User: {$post->user->name} Post: {$post->title} <br/>";
+    // }
+
+    // With function:
+    // $posts = Post::with(["user"])->get();
+    // foreach ($posts as $post) {
+    //     echo "User: {$post->user->name} Post: {$post->title} <br/>";
+    // }
+
+    // lazy loading (n+1 problem)
+    $posts = Post::all();
+    foreach ($posts as $post) {
+        echo "User: {$post->user->name} Post: {$post->title} <br/>";
+    }
+
+    //->pluck('content');
     // return view('welcome');
 });
